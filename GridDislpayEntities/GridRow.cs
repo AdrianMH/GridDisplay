82f1c096183929f2b4c110a5a3eefff990019007
AdrianMH <adrianhomone@gmail.com>
2015-02-09 14:13:49 +0200

    Working

---
+++ GridDislpayEntities/GridRow.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace GridDisplayService.Contracts
+{
+    [DataContract]
+    public class GridRow
+    {
+        [DataMember]
+        public int ProductId { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public string CategoryName { get; set; }
+        [DataMember]
+        public decimal Price { get; set; }
+    }
+}
