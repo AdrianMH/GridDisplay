d5bdafd821f5788acc35bb9960bea0825213d43c
AdrianMH <adrianhomone@gmail.com>
2015-02-10 15:30:04 +0200

    Almost

---
+++ GridDisplayClient/Controllers/DbController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace GridDisplayClient.Controllers
+{
+    public class DbController : Controller
+    {
+        // GET: Db
+        public ActionResult Index()
+        {
+            ServiceReference1.GridDisplayClient obj = new ServiceReference1.GridDisplayClient();
+            return View(obj.GetProducts());
+        }
+    }
+}
