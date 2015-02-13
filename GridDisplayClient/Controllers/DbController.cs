b09e8b5b90605d0b0782b01a4d7ba5e0ee0914f6
AdrianMH <adrianhomone@gmail.com>
2015-02-13 11:45:07 +0200

    New branch

---
+++ GridDisplayClient/Controllers/DbController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace GridDisplayClient.Controllers
+{
+    public class DbController : Controller
+    {
+        // GET: Db
+        public ActionResult Index()
+        {
+            ServiceReference1.GridDisplayClient obj = new ServiceReference1.GridDisplayClient();
+            return View("view",obj.GetProducts("search"));
+        }
+
+        public ActionResult Archive(int productId = 0)
+        {
+            ServiceReference1.GridDisplayClient archive = new ServiceReference1.GridDisplayClient();
+            archive.Archive(productId);
+            return View("view");
+        }
+    }
+}
